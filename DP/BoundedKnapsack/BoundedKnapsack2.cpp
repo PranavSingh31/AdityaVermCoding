8f25b4a987ed49f5903b9fe8c5cb66e3729743aa
P S <prsingh1347@gmail.com>
2022-10-23 11:47:04 +0530

    Add files via upload

---
+++ DP/BoundedKnapsack/BoundedKnapsack2.cpp
@@ -0,0 +1,29 @@
+//bottom-up
+#include<bits/stdc++.h>
+using namespace std;
+
+int knapsack(int wt[],int val[],int w,int n){
+//initializing dp table
+int dp[n+1][w+1];
+memset(dp,-1,sizeof(dp));
+
+for(int i=0;i<n+1;i++){
+    for(int j=0;j<w+1;j++){
+        if(i==0 || j==0) dp[i][j]=0; //base condition
+        else{ //branch condition
+            if(wt[i-1]<=j) dp[i][j] = max(val[i-1]+dp[i][j-wt[i-1]],dp[i-1][j]);
+            else if(wt[i-1]>j) dp[i][j]=dp[i-1][j];
+        }
+    }
+}
+return dp[n][w];
+}
+
+int main(){
+    int wt[]={1,3,4,5};
+    int val[]={10,40,50,70};
+    int w=8;
+    int n=sizeof(wt)/sizeof(wt[0]); 
+    cout<<knapsack(wt,val,w,n);
+    return 0;
+}
---
+++ DP/BoundedKnapsack/CoinChangeMaxWays.cpp
@@ -0,0 +1,29 @@
+//bottom-up approach
+#include<bits/stdc++.h>
+using namespace std;
+
+int subset(int arr[], int s,int n){
+//initializing dp table
+int dp[n+1][s+1];
+memset(dp,-1,sizeof(dp));
+
+//base condition
+for(int i=0;i<n+1;i++){
+    for(int j=0;j<s+1;j++){
+        if(j==0) dp[i][j]=1;
+        else if(i==0 && j!=0) dp[i][j]=0;
+        else{
+            if(arr[i-1]<=j) dp[i][j]= (dp[i][j-arr[i-1]] + dp[i-1][j]);
+            if(arr[i-1]>j) dp[i][j]=dp[i-1][j];
+        }
+    }
+}
+return dp[n][s];
+}
+int main(){
+    int arr[]={1,2,3};
+    int w=5;
+    int n=sizeof(arr)/sizeof(arr[0]);
+    cout<<subset(arr,w,n);
+    return 0;
+}
